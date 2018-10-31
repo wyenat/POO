2cef02ee2536191ea6371301d6d8291387251f6d
Vivien Matter <vivien.matterspc@gmail.com>
2018-10-31 16:02:27 +0100

    Utilisatio étrange des enum

--- src/TestEvenement.java
+++ src/TestEvenement.java
@@ -1,12 +1,16 @@
 
 import io.*;
+import io.NatureTerrain;
 
 public class TestEvenement {
 
     public static void main(String[] args) {
         Robot Lucille = new Robot(0, 0 ,10);
         Robot Vivien = new Robotdrone(0, 0 ,10);
-        Case C = new Case(12, 12, EAU);
+        NatureTerrain nature;
+        String mmnature = "EAU";
+        nature = NatureTerrain.valueOf(mmnature);
+        Case C = new Case(12, 12, nature);
         Evenementdeplacement Deplacer = new Evenementdeplacement(14, Vivien, C);
         Deplacer.execute();
         System.out.println(Lucille.GetLigne());
--- src/io/Evenement.java
+++ src/io/Evenement.java
@@ -1,4 +1,5 @@
 package io;
+import java.io.*;
 
 public class Evenement{
   private long Datefin;
