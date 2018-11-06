e86fe61aecd1c129e9528167f513d12b7403373c
Enguerran Debeaupuis <Enguerran.Debeaupuis@grenoble-inp.org>
2018-11-06 14:47:09 +0100

    Concatenation de code

--- src/io/Evenementdeplacement.java
+++ src/io/Evenementdeplacement.java
@@ -27,32 +27,7 @@ public class Evenementdeplacement extends Evenement {
     if (map.voisinExiste(C, this.direction)){
         this.Case = map.GetVoisin(C, this.direction);
         super.robot = robot;
-        boolean possible = false;
-        switch (this.robot.GetTypeRobot()) {
-          case ROUES:
-          Robotaroues Robot_roue = new Robotaroues(robot.GetLigne(), robot.GetColonne(), robot.GetVitesse());
-          possible = Robot_roue.test_deplacement(this.Case);
-          break;
-
-          case CHENILLES:
-          Robotachenilles Robot_chenille = new Robotachenilles(robot.GetLigne(), robot.GetColonne(), robot.GetVitesse());
-          possible = Robot_chenille.test_deplacement(this.Case);
-          break;
-
-          case PATTES:
-          Robotapattes Robot_pattes = new Robotapattes(robot.GetLigne(), robot.GetColonne(), robot.GetVitesse());
-          possible = Robot_pattes.test_deplacement(this.Case);
-          break;
-
-          case DRONE:
-          Robotdrone Robot_drone = new Robotdrone(robot.GetLigne(), robot.GetColonne(), robot.GetVitesse());
-          possible = Robot_drone.test_deplacement(this.Case);
-          break;
-
-          default:
-          break;
-
-        }
+        boolean possible = robot.test_deplacement(this.Case);
 
         if (possible){
           super.getRobot().setPosition(this.Case);
@@ -62,37 +37,7 @@ public class Evenementdeplacement extends Evenement {
       throw new IllegalArgumentException("Le robot ne peut pas aller la");
     }
   }
-  public void deplacement(Robot robot, Case C, DonneesSimulation donnees){
-
-    //   switch (robot.GetTypeRobot()) {
-    //     case ROUES:
-    //       Robotaroues Robot_roue = new Robotaroues(Robot.GetLigne(), Robot.GetColonne(), Robot.GetVitesse());
-    //       break;
-    //
-    //     case CHENILLES:
-    //       Robotachenilles Robot_chenille = new Robotachenilles(Robot.GetLigne(), Robot.GetColonne(), Robot.GetVitesse());
-    //       break;
-    //
-    //     case PATTES:
-    //       Robotapattes Robot_pattes = new Robotapattes(Robot.GetLigne(), Robot.GetColonne(), Robot.GetVitesse());
-    //       break;
-    //
-    //     case DRONE:
-    //       Robotdrone Robot_drone = new Robotdrone(Robot.GetLigne(), Robot.GetColonne(), Robot.GetVitesse());
-    //       Case[] Trajet = Robot_drone.calcul_deplacement(robot, C, donnees);
-    //       for (int i =0; i<50; i++){
-    //       System.out.println(Trajet[i]);
-    //       }
-    //       break;
-    //
-    //     default:
-    //       System.out.println("AIE");
-    //       break;
-    //
-    //   }
-    //
-    //
-    }
+  
 
     @Override
     public String toString(){
--- src/io/Robotdrone.java
+++ src/io/Robotdrone.java
@@ -82,64 +82,8 @@ public class Robotdrone extends Robot{
     return false;
   }
 
-
-
   public boolean test_deplacement(Case C){
     return true;
   }
-
-
-  public void calcul_deplacement(Robot robot, Case C, DonneesSimulation donnees){
-      // NatureTerrain nature;
-      // String mmnature = "EAU";
-      // nature = NatureTerrain.valueOf(mmnature);
-      // Carte carte = donnees.GetCarte();
-      // int nb_lignes =100;// carte.GetNbLignes();
-      // int nb_colonnes = 100;//carte.GetNbColonnes();
-      // int ligne_arrivee = C.GetLigne();
-      // int colonne_arrivee = C.GetColonne();
-      // int ligne_depart = robot.GetLigne();
-      // int colonne_depart = robot.GetColonne();
-      // Case[] Tableau = new Case[1000];//(0, 0, nature);//[ecart_col*ecart_lig];
-      // int nb_cases = 0;
-      // int sum_lig = 1;
-      // int sum_col = 1;
-      //
-      //
-      // int ecart_lig = ligne_arrivee - ligne_depart;
-      // if (ligne_arrivee < ligne_depart){
-      //   sum_lig = -1;
-      //   ecart_lig = -ecart_lig;
-      // }
-      // int ecart_col = colonne_arrivee - colonne_depart;
-      // if (colonne_arrivee < colonne_depart){
-      //   sum_col = -1;
-      //   ecart_col = -ecart_col;
-      // }
-      //
-      //
-      // if (ligne_arrivee < nb_lignes && ligne_depart < nb_colonnes){
-      //     while (ligne_depart != ligne_arrivee){
-      //       Case c = new Case(ligne_depart, colonne_depart, nature);
-      //       Tableau[nb_cases] = c;
-      //       ligne_depart = ligne_depart + sum_lig;
-      //       nb_cases++;
-      //     }
-      //     while (colonne_depart != colonne_arrivee){
-      //       Case c = new Case(ligne_depart, colonne_depart, nature);
-      //       Tableau[nb_cases] = c;
-      //       colonne_depart = colonne_depart + sum_col;
-      //       nb_cases++;
-      //     }
-      //     Case c = new Case(ligne_depart, colonne_depart, nature);
-      //     Tableau[nb_cases] = c;
-      //     colonne_depart = colonne_depart + sum_col;
-      //     nb_cases++;
-      // }
-      // else {
-      //   System.out.println("AAAH");
-      // }
-      // return Tableau;
-    }
 
 }
